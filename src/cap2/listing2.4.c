aef660a31f3254c0da95a69196e15bebdb747953
BraianRom <isaiasrom123@gmail.com>
2025-08-25 20:47:25 -0400

    Cap2: agregados listings 2.3 (print-env) y 2.4 (client)

---
+++ src/cap2/listing2.4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char* server_name = getenv("SERVER_NAME");
+    if (server_name == NULL)
+        /* Use the default if not set. */
+        server_name = "server.my-company.com";
+
+    printf("accessing server %s\n", server_name);
+
+    /* Access the server here... */
+
+    return 0;
+}
